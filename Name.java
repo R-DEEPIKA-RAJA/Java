6af42631f33456c87e7d7e231e3625777ec84c90
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-08 22:14:36 +0530

    DAY1

---
+++ Name.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+************************************************************************
+* 
+*/
+class Name
+{
+    public static void main (String[] args) 
+    {
+        System.out.print("deepika");
+    }
+}
