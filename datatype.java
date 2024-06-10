8cb09e765b73494826daa2216e3fd790a0db536b
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-10 21:26:27 +0530

    Day2

---
+++ datatype.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class datatype
+{
+	public static void main(String[] args) {
+	    int a = 2000000000;
+	    long b =2000000000;
+	    byte c= 127;
+	    short d=32000;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+}
