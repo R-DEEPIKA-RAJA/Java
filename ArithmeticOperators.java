f2a32bfd74cd035c777615b74fb3b88589c05bba
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-20 14:32:32 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class ArithmeticOperators
+{
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=a/b;
+        int g=a%b;
+        System.out.println(c);
+         System.out.println(d);
+         System.out.println(e);
+         System.out.println(f);
+          System.out.println(g);
+        
+    }
+}
