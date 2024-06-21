766bd5af3dc6278f7ea5d7c0f5f020b88e92a2d8
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-21 22:10:07 +0530

    Day4

---
+++ Inc.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Inc
+{
+	public static void main(String[] args)
+	{
+	     int a=4;
+		System.out.println(a++);
+		++a;
+		System.out.println(a);
+	}
+}
