fb2525b13eff123ff764b7428dc64fa9c45154c3
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-22 18:28:56 +0530

    Day5

---
+++ Rel.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Rel
+{
+	public static void main(String[] args) {
+	    
+	    int a=10;
+	    int b=5;
+	    
+	    	System.out.println(a>b);
+	    		System.out.println(a<b);
+	    			System.out.println(a>=b);
+	    	System.out.println(a<=b);
+	    	System.out.println(a!=b);
+		System.out.println(a==b);
+	}
+}
