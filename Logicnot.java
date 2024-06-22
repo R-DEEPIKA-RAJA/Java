52ae8d4f82d611f1a39299b05c5f176556b731d1
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-22 18:29:22 +0530

    Day5

---
+++ Logicnot.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Logicnot
+{
+	public static void main(String[] args) {
+	    
+	    boolean a=true;
+	    boolean b=false;
+	    	System.out.println(!a);
+	    		System.out.println(!b);
+	}
+}
