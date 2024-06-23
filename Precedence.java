102eac484abdab54d5a69e1efbf2f0cd2a314159
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-23 21:27:48 +0530

    Day6

---
+++ Precedence.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Precedence
+{
+	public static void main(String[] args) {
+	    int d=2;
+	    int b=5;
+	    int c=10;
+	    int a;
+	    a=c/d*b;
+	    	System.out.println(a);
+	    	a=c*d+b;
+	    		System.out.println(a);
+	    		a=c/d*b+c-d;
+	    			System.out.println(a);
+	    	
+	}
+}
