4f1cd64e484f9fae98a1fc0bea181ddd0d4cecb9
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-23 21:27:02 +0530

    Day6

---
+++ Logicand.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Logicand
+{
+	public static void main(String[] args) {
+	    
+	    boolean a=true;
+	    boolean b=false;
+	    	System.out.println(a&&b);
+	    	
+	}
+}
