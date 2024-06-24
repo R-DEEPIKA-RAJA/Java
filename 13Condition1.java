63cb129aa9381b72d31b86331c8b3023ec19077c
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-24 13:16:52 +0530

    Day7

---
+++ 13Condition1.java
@@ -0,0 +1,23 @@
+
+public class Condition
+{
+	public static void main(String[] args)
+	{
+	   boolean attend=true;
+	   if(attend==true)
+	   {
+	       System.out.println("class attended");
+	   }
+	   else
+	   {
+	       System.out.println("not attended");
+	   }
+	}
+}
+	    
+	    
+	    
+	    
+	    
+	    
+	    
