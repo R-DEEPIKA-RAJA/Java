e3c0ec212fdcfe33320d4fc6a6e52d0ed434a298
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-24 13:17:27 +0530

    Day7

---
+++ 14Condition2.java
@@ -0,0 +1,28 @@
+
+public class Condition2
+{
+	public static void main(String[] args)
+	{
+	   char attend= 'a';
+	   if(attend=='p')
+	   {
+	       System.out.println("class attended");
+	   }
+	   else if(attend=='o')
+	   {
+	       System.out.println("on duty");
+	   }
+	   else
+	   {
+	       System.out.println("not attended");
+	   }
+	   
+	}
+}
+	    
+	    
+	    
+	    
+	    
+	    
+	    
