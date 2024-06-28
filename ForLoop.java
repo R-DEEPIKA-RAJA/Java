b64b8b62ae7fc788d2df1ebacf90db3e4ff5de68
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-28 10:25:05 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,15 @@
+ public class  ForLoop
+ {
+    public static void main(String[]args)
+    {
+       int i;
+       int n=15;
+       for(i=0;i<=n;i++)
+       {
+           System.out.println(i);
+       }
+    
+        
+        
+    }
+}
