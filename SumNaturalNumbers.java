285810084196ad62f50118adee0d99fb0ac002db
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-28 10:25:22 +0530

    Add files via upload

---
+++ SumNaturalNumbers.java
@@ -0,0 +1,17 @@
+ public class  SumNaturalNumbers
+ {
+    public static void main(String[]args)
+    {
+       int i;
+       int sum=0;
+       int n=15;
+       for(i=1;i<=n;i++)
+       {
+           sum+=i;
+           System.out.println(sum);
+       }
+    
+        System.out.println("the sum is "+sum);
+        
+    }
+}
