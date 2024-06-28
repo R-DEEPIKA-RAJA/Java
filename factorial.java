f26e0b40b1b51a06df0d51bcf770b7546c1bdd23
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-28 10:26:00 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial
+{
+   public static void main (String[] args) 
+   {
+       int c=1;
+    int n=5;
+    for(int i=1;i<6;i++)
+    {
+          c=c*i;
+        System.out.println(c);
+    }
+    System.out.println("the factorial of 5 is "+c);
+   }
+}
