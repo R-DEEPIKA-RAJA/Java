a27494769cb2d92fe44aa3c41b259926b7ef8816
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-06-28 10:28:32 +0530

    Add files via upload

---
+++ array (1).java
@@ -0,0 +1,10 @@
+public class array
+{
+    public static void main (String[] args) 
+    {
+        char[] rollno= new char[50];
+        rollno[0]='d';
+        rollno[1]='a';
+        System.out.println(rollno[0]);
+    }
+}
