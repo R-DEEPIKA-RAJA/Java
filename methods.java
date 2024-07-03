4d349bfd9c170d32cb9e01efc1fdbf1f2a26c6c4
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-07-03 11:18:59 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,15 @@
+public class methods
+{
+    public static void main (String[] args)
+    {
+        add();
+        System.out.println("c is printed");
+    }
+    public static void add()
+    {
+        int a=10;
+        int b=4;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
