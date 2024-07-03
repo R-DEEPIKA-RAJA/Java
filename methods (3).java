177780f57da1def72cf921cfce03e28022af8529
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-07-03 11:19:32 +0530

    Add files via upload

---
+++ methods (3).java
@@ -0,0 +1,14 @@
+public class methods
+{
+    public static void  main (String[] args)
+    {
+        natprint(6);
+    }
+    public static void natprint(int n)
+    {
+        for(int i =1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
