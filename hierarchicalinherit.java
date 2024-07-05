3ce2e74f72c1a16ecc14a2c7f653c5c01dd6bbb2
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2024-07-05 10:40:16 +0530

    Add files via upload

---
+++ hierarchicalinherit.java
@@ -0,0 +1,36 @@
+class animal
+{
+    void display()
+    {
+        int leg=4;
+        System.out.println("i can eat and walk");
+    }
+}
+
+class dogs extends animal{
+    void display0()
+    {
+        System.out.println(" i have 4 legs ");
+    }
+}
+
+class lion extends animal{
+    void display1()
+    {
+        System.out.println("i can roar");
+    }
+}
+
+class hierarchicalinherit
+{
+    public static void main (String[] args) {
+    animal obj  = new animal();
+    dogs obj1 = new dogs();
+    lion obj2 = new lion();
+    obj1.display0();
+    obj.display();
+    obj1.display();
+    obj2.display();
+    obj2.display1();
+    }
+}
